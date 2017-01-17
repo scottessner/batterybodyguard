c9ad0615451a1ca9662d8ad0c18894a29bfe3ba7
scottessner <scott.essner@gmail.com>
2017-01-16 23:17:41 -0600

    Moved auth templates and updated auth urls to django standard

--- batteries/views.py
+++ batteries/views.py
@@ -15,6 +15,12 @@ class BatteryCreate(CreateView):
     model = Battery
     fields = ['cells', 'capacity', 'nickname', 'barcode', 'cell_voltage']
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(BatteryCreate, self).get_context_data(**kwargs)
+        context['task'] = 'Create'
+        return context
+
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(BatteryCreate, self).form_valid(form)
@@ -24,6 +30,12 @@ class BatteryCreate(CreateView):
 class BatteryUpdate(UpdateView):
     model = Battery
     fields = ['cells', 'capacity', 'nickname', 'barcode', 'cell_voltage']
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(BatteryUpdate, self).get_context_data(**kwargs)
+        context['task'] = 'Update'
+        return context
 
 
 @method_decorator(login_required, name='dispatch')
@@ -35,7 +47,7 @@ class BatteryDetail(DetailView):
         # Call the base implementation first to get a context
         context = super(BatteryDetail, self).get_context_data(**kwargs)
         # Add in a QuerySet of all the books
-        context['battery_list'] = Battery.objects.all()
+        # context['battery_list'] = Battery.objects.all()
         return context
 
 
