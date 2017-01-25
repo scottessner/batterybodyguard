f949b7704c25250a514610890a0f2d05eae0e265
sjessn <scott.j.essner@monsanto.com>
2017-01-25 17:49:57 -0600

    Hacked basic framework for functional tests

--- batteries/models.py
+++ batteries/models.py
@@ -7,12 +7,21 @@ import uuid
 class Battery(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    cells = models.PositiveSmallIntegerField()
     capacity = models.PositiveIntegerField()
     nickname = models.CharField(max_length=200)
     barcode = models.CharField(max_length=100, blank=True)
-    cell_voltage = models.DecimalField(max_digits=3, decimal_places=2)
-
+    max_charge_rate = models.IntegerField()
+    brand = models.CharField(max_length=100)
 
     def get_absolute_url(self):
         return reverse('battery-detail', kwargs={'pk': str(self.pk)})
+
+
+class Cell(models.Model):
+    battery = models.ForeignKey(Battery, on_delete=models.CASCADE)
+    voltage = models.DecimalField(max_digits=3, decimal_places=2)
+
+
+class Equipment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- functional_tests/test_user_creation.py
+++ functional_tests/test_user_creation.py
@@ -12,5 +12,21 @@ class NewVisitorTest(FunctionalTest):
         # He sees the page title and verifies it's a battery site
         self.assertIn('Battery Bodyguard', self.browser.title)
 
-        time.sleep(10)
+        # He decides that he would like to create an account He clicks the signup link
+        self.browser.find_element_by_id('a_signup').click()
 
+        # He enters a username and password into the form
+        user_field = self.browser.find_element_by_id('id_username')
+        user_field.send_keys('iamauser')
+
+        pw1_field = self.browser.find_element_by_id('id_password1')
+        pw1_field.send_keys('P@ssw0rd')
+        pw2_field = self.browser.find_element_by_id('id_password2')
+        pw2_field.send_keys('P@ssw0rd')
+
+        # He clicks submit to confirm creation
+        self.browser.find_element_by_id('b_signup').click()
+
+        # He verifies that the home page shows that he is logged in
+        user_text = self.browser.find_element_by_id('logged_in_user')
+        self.assertIn('iamauser', user_text.text)
